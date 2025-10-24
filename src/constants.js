5321ea48455e2a69504fa6259b64dcac8e91991e
Suki <tues1205@naver.com>
2025-10-25 02:18:45 +0900

    feat(utils): 랜덤 숫자 생성 함수

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// 랜덤 값 범위
+export const RANDOM_MIN = 0; // 최소
+export const RANDOM_MAX = 9; // 최대
+
+// 자동차가 전진하는 기준 값
+export const MOVE_STANDARD = 4;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import { Random } from "@woowacourse/mission-utils";
+import { RANDOM_MAX, RANDOM_MIN } from "../constants.js";
 import Car from "../domain/Car.js";
 
 // 문자열을 쉼표(,) 기준으로 나누기, 공백 제거
@@ -13,4 +15,9 @@ export function createCars(names) {
 // 시도 횟수 입력값을 정수로 변환
 export function parseTryCount(count) {
   return Number(count);
+}
+
+// 랜덤 숫자 생성
+export function getRandomNumber() {
+  return Random.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
 }

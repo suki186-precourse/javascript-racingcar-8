c951b4481fb27a1f48b980e2109174cec638ee02
Suki <tues1205@naver.com>
2025-10-25 02:38:50 +0900

    feat(Car): 자동차 이동거리를 문자열(-)로 표시

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,4 +1,3 @@
-import { Console } from "@woowacourse/mission-utils";
 import { MOVE_STANDARD } from "../constants.js";
 import { getRandomNumber } from "../utils/utils.js";
 
@@ -13,9 +12,15 @@ export default class Car {
   move() {
     const randomNumber = getRandomNumber(); // 난수 생성
 
-    // 랜던 값이 기준 값보다 크다면 전진
+    // 랜덤 값이 기준 값보다 크다면 전진
     if (randomNumber >= MOVE_STANDARD) {
       this.position += 1;
     }
+  }
+
+  // 현재 위치만큼 문자열로(-) 표시
+  printDistance() {
+    const distance = "-".repeat(this.position);
+    return `${this.name} : ${distance}`;
   }
 }

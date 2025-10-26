309ee7c662f602b4040d9e3cf2749251d0f8f862
Suki <tues1205@naver.com>
2025-10-26 21:20:44 +0900

    feat(utils): 구분자 유효성 검사

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,8 @@
-import { NAME_DELIMITER } from "../constants.js";
+import {
+  ERROR_MESSAGE,
+  INVALID_CAR_NAME_REGEX,
+  NAME_DELIMITER,
+} from "../constants.js";
 import Car from "../domain/Car.js";
 
 // 문자열을 쉼표(,) 기준으로 나누기, 공백 제거
@@ -14,4 +18,11 @@ export function createCars(names) {
 // 시도 횟수 입력값을 정수로 변환
 export function parseTryCount(count) {
   return Number(count);
+}
+
+// 자동차 이름 입력값에 한글, 영문, 숫자, 쉼표 외 다른 문자가 있는 경우
+export function validateCarName(input) {
+  if (!INVALID_CAR_NAME_REGEX.test(input)) {
+    throw new Error(ERROR_MESSAGE.INVALID_DELIMITER);
+  }
 }
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
-import { parseCarNameByComma, parseTryCount } from "../utils/utils.js";
+import {
+  parseCarNameByComma,
+  parseTryCount,
+  validateCarName,
+} from "../utils/utils.js";
 
 export const InputView = {
   // ===== 자동차 이름 입력 및 구분자 기준으로 분리
@@ -8,6 +12,9 @@ export const InputView = {
     const carNamesInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+
+    // 문자열 유효성 검사
+    validateCarName(carNamesInput);
 
     // 이름들을 구분자(,) 기준으로 분리, 리스트로 반환
     return parseCarNameByComma(carNamesInput);

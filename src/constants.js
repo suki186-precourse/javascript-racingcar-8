806b41121096787de24b5c8050bede82fd7c2b0b
Suki <tues1205@naver.com>
2026-01-05 18:04:23 +0900

    feat(domain): Car 도메인 기본 코드 작성

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
-// 입력 메세지
+// ===== 입력 메세지
 export const INPUT_MESSAGES = Object.freeze({
   ENTER_CAR_NAMES:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   ENTER_MOVE_COUNT: "시도할 횟수는 몇 회인가요?\n",
 });
 
-// 출력 메세지
+// ===== 출력 메세지
 export const OUTPUT_MESSAGES = Object.freeze({
   RESULT_INFO: "\n실행 결과\n",
   ROUND_RESULT: (name, distance) => `${name} : ${"-".repeat(distance)}`,
@@ -16,7 +16,13 @@ export const COMMON = Object.freeze({
   DELIMITERS: ",",
 });
 
-// 에러 메세지
+// ===== 자동차
+export const CAR_LOGIC = Object.freeze({
+  MOVE_STANDARD: 4,
+  MOVE_STEP: 1,
+});
+
+// ===== 에러 메세지
 export const ERROR = "[ERROR]";
 
 export const ERROR_MESSAGES = Object.freeze({
---
+++ src/domain/Car.js
@@ -0,0 +1,29 @@
+import { CAR_LOGIC } from "../constants";
+
+class Car {
+  #name;
+  #position;
+
+  constructor(name) {
+    // 이름 유효성 검증 (예정)
+    this.#name = name; // 이름
+    this.#position = 0; // 현재 위치
+  }
+
+  // getter
+  get name() {
+    return this.#name;
+  }
+
+  get position() {
+    return this.#position;
+  }
+
+  // 이동: 랜덤 값이 기준 값보다 크다면 전진
+  move(number) {
+    if (number >= CAR_LOGIC.MOVE_STANDARD) {
+      this.#position += CAR_LOGIC.MOVE_STEP;
+    }
+  }
+}
+export default Car;
